f6f6dbe504e2899cb969e0ad0e498c2323f54102
adamstone333 <adamstone333@gmail.com>
2017-10-25 19:23:18 +0530

    Create vbox.sh

---
+++ vbox.sh
@@ -0,0 +1,11 @@
+sudo apt-get install gcc g++ bcc iasl xsltproc uuid-dev zlib1g-dev libidl-dev \
+                libsdl1.2-dev libxcursor-dev libasound2-dev libstdc++5 \
+                libhal-dev libpulse-dev libxml2-dev libxslt1-dev \
+                python-dev libqt4-dev qt4-dev-tools libcap-dev \
+                libxmu-dev mesa-common-dev libglu1-mesa-dev \
+                linux-kernel-headers libcurl4-openssl-dev libpam0g-dev \
+                libxrandr-dev libxinerama-dev libqt4-opengl-dev makeself \
+                libdevmapper-dev default-jdk python-central \
+                texlive-latex-base \
+                texlive-latex-extra texlive-latex-recommended \
+                texlive-fonts-extra texlive-fonts-recommended
